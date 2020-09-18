ea767f0054a010d8e102a9732efb348222ee419f
nur312 <nurzhigit312@gmail.com>
2020-09-18 22:47:02 +0300

    Examples with screenshots

---
+++ HotFinger-master/README.md
@@ -0,0 +1,176 @@
+You know that insecure fingerprint reader next to your laptop keyboard? Now you
+can turn it into something marginally useful by repurposing it as an application
+launcher with **HotFinger** (like hot*key* but for *finger*s, get it?)
+
+![Screenshot](https://i.imgur.com/VAwqHb0.png)
+
+System requirements include Windows 10 operating system and a
+[WBF-compatible](https://docs.microsoft.com/en-us/windows/desktop/secbiomet/biometric-service-api-portal)
+fingerprint reader.
+
+[Download the latest version
+here](https://github.com/resilar/HotFinger/releases/latest). Happy fingering!
+
+
+## Features
+
+- Bind commands to fingerprints
+  - Supported commands include ...
+    - Launching an application (with or without arguments)
+    - Opening directory in the Windows file browser
+    - Opening URL in your favorite web browser
+    - Whatever else
+      [`ShellExecute()`](https://docs.microsoft.com/en-us/windows/desktop/api/shellapi/nf-shellapi-shellexecutea)
+      can do
+  - All **ten** fingers supported!
+- Written in assembler
+  - So it's blazingly fast
+  - And crazy small (42KB including an 18KB icon)
+- High DPI aware GUI (come on, it's 2019)
+- Uninstaller that sucks less
+  - *Clean* uninstalls
+    - No config files left lying around
+    - ... nor WinBio database files
+    - ... nor registry settings
+  - Trigger via the menu: `Options > Uninstall`
+- Minimize to tray
+
+
+## Requirements
+
+- **Windows 10** \
+  Windows 8 is unsupported because HotFinger uses a few DPI-aware WinAPI
+  functions that were first introduced in Windows 10 (version 1607). Thus,
+  starting HotFinger on Windows 8 gives an error message titled: *Entry Point
+  Not Found*. However, implementing Windows 8 support is a straightforward task:
+  simply call DPI-**un**aware versions of the WinAPI functions if DPI-aware
+  versions are unavailable. If someone implements Windows 8 support
+  [cleverly](https://docs.microsoft.com/en-us/windows/desktop/Dlls/using-run-time-dynamic-linking)
+  (that is, without affecting the high-DPI scaling behavior on Windows 10), feel
+  free to submit a pull request to this repository.
+
+  Similarly, Windows 7 is unsupported because of the missing DPI-aware WinAPIs.
+  Windows 7 also lacks `WinBioAsync*` family of functions and asynchronous
+  WinBio sessions added in Windows 8 and used by HotFinger. Writing a
+  compatibility layer for Windows 7 is hence more difficult, although still
+  feasible with major effort. The basic idea is to emulate asynchronous WinBio
+  sessions using threads.
+
+- **Fingerprint reader** (compatible with [Windows Biometric
+  Framework](https://docs.microsoft.com/en-us/windows/desktop/secbiomet/biometric-service-api-portal)) \
+  There is always a good chance that the program will not work because of some
+  subtle difference in your specific fingerprint reader or associated driver(s).
+  If so, [opening a new issue](https://github.com/resilar/HotFinger/issues/new)
+  would be highly appreciated. Remember to include error messages and other
+  debug information if possible.
+
+- **Sane antivirus software** \
+  Unfortunately, [6/70 engines in
+  VirusTotal](https://www.virustotal.com/#/file/1890234c881855c37c99eeb90599b5cd590c77fa95a7954468032b60ab2e91e4/detection)
+  flag `hotfinger.exe` as malicious. Out of these, 2 are false detections of
+  `Win32:MalOb-IJ [cryp]` and the remaining 4 are false positives reported by
+  heuristics. This normally would not be a big deal, but it turns out that the 6
+  antivirus engines falsely detecting `hotfinger.exe` include popular software
+  such as Avast, AVG, Cylance and Symantec. Therefore, add an exception for
+  `hotfinger.exe` in your antivirus if you use one of those products.
+
+  **Update**: Fuck. It's 25/68 VirusTotal detections now. Apparently Bitdefender
+  classified `hotfinger.exe` as malware and created `Trojan.GenericKD.40939660`
+  signature for it (probably thanks to [Bitdefender machine learning based
+  threat detection](https://www.bitdefender.co.th/wp-content/uploads/gz/Bitdefender-2017-TechnicalBrief-MachineLearning-crea2103-A4-en-EN-2-GenericUse.pdf)
+  which misclassifies handwritten assembly code as "abnormal" and, therefore,
+  malicious by some retarded logic). The false positive detection rate shoots up
+  to 420% because nearly every other antivirus product on the market uses the
+  same Bitdefender scan engine and signature database.
+
+
+## Geeks
+
+Compile: `fasm hotfinger.asm` (download FASM for Windows
+[here](https://flatassembler.net/download.php)).
+
+Contributors please use the flat editor `FASMW.EXE` to format your code.
+
+
+## Q&A
+
+"Boomer x86 assembly coder rants about modern technology" section disguised as
+"Questions and Answers". Please, for your own sake, stop reading here.
+
+
+#### Why x86 assembly in ~~2018~~THE CURRENT YEAR?
+
+Mostly as a demonstration to show that it is still possible to write GUI
+applications for Windows without tons of bloated libraries and dependencies.
+Also because x86 assembly felt like a good match for this particular project;
+thanks to the nice 32-bit WinAPI interface of WinBio offered by Windows.
+Ultimately, writing x86 assembly is not more difficult than writing C code, so
+why the hell not?
+
+
+#### Why FASM instead of MASM or NASM?
+
+My unbiased objective opinion: FASM is the best assembler out there, hands down.
+It is the only assembler that truly gives the programmer total byte-level
+control of the output. This is largely because FASM produces the target
+executable directly, bypassing an external linker program required by other
+assemblers. FASM is fully self-hosting which makes it extremely fast compared to
+assemblers written in higher-level languages such as C. Finally, FASM has the
+most powerful macro system by far.
+
+OS developers may write bootloaders and interrupt handlers in MASM or NASM,
+whereas full operating systems are typically written in FASM (see
+[DexOS](http://dex-os.github.io/), [KolibriOS](http://www.kolibrios.org/en/),
+[MenuetOS](http://menuetos.net/), ...). In other words, FASM is the
+assembler-of-choice for large x86 assembly projects, while shitty assemblers can
+be bearable in small projects like single functions.
+
+MASM syntax is braindead for obfuscating memory operands (e.g., `LEA` and
+`offset` stupidity); for fuck's sake, do not hide memory references from the
+programmer, ever. NASM is acceptable, although relying on an external linker
+sucks. GAS and AT&T syntax are full retard.
+
+See also: [flat assembler - Design Principles (or why flat assembler is
+different)](https://flatassembler.net/docs.php?article=design).
+
+
+#### Why not 64 bit?
+
+All common x86-64 calling conventions have been designed primarily for
+compilers, in a way that makes function calls unintuitive and painful when
+hand-writing assembly. For example, all common x86-64 calling conventions apply
+fastcall-style argument passing, which improves the performance compared to
+stdcall on average, but makes the caller responsible for reserving registers for
+function calls. Consequently, the poor assembly programmer has fewer registers
+available to use without spilling. Compilers do not care, of course, because
+register allocation boils down to the same graph coloring problem anyways.
+
+Microsoft x64 calling convention also requires that the caller allocates a
+*shadow space*, i.e., 32 bytes of space from the top of the stack before a
+function call. The space is intended for saving 4 fastcall-registers in order to
+simplify the compiler support for C/C++ functions
+([source](https://msdn.microsoft.com/en-us/library/ms235286.aspx)). For some
+insane reason, the 32-byte shadow space is required even if the callee function
+takes fewer than 4 fastcall arguments. Moreover, the fifth and subsequent
+arguments are pushed onto the stack **on the top of the shadow space**, which
+complicates the mess even further. As a result, the calling convention is very
+cumbersome for humans writing x86-64 assembly by hand (macros can help a bit).
+
+System V AMD64 ABI does not suffer from the shadow space madness. However, it
+reserves `RSI` and `RDI` registers for passing fastcall arguments. This is a bad
+design decision since `RSI` and `RDI` are treated as special *source* and
+*destination* registers in the x86-64 instruction set. For example, the usage of
+x86-64 string instructions (e.g., `LODSB`, `STOSW`, `CMPSD`) becomes more
+annoying in some situations as a consequence.
+
+In comparison, Win32 stdcall calling convention feels luxuriously easy and
+intuitive from the perspective of an x86 programmer. Additionally, the
+stack-based argument passing of stdcall often produces more compact code (for
+example, the encoding of `MOV ECX, 42` is 5 bytes long, whereas `PUSH 42` takes
+only 2 bytes). Shorter instruction encodings can yield significant performance
+improvements due to potentially more efficient cache utilization.
+
+
+#### Why no `invoke` or control-flow macros?
+
+Macros are for pussies. Just give up and go fucking write JavaScript or Python.
---
+++ HotFinger-master/hotfinger.asm
@@ -0,0 +1,497 @@
+format PE GUI 4.0
+
+include 'win32a.inc'
+include 'winbio.inc'
+
+;-------------------------------------------------------------------------
+; Program settings
+;-------------------------------------------------------------------------
+struct GUI_SETTINGS
+  x  dd ?
+  y  dd ?
+  cx dd ?
+  cy dd ?
+  dwMinimized dd ?
+  dwMaximized dd ?
+ends
+
+struct COMMAND_SETTINGS
+  RThumb  db MAX_PATH dup (?)
+  RIndex  db MAX_PATH dup (?)
+  RMiddle db MAX_PATH dup (?)
+  RRing   db MAX_PATH dup (?)
+  RLittle db MAX_PATH dup (?)
+  LThumb  db MAX_PATH dup (?)
+  LIndex  db MAX_PATH dup (?)
+  LMiddle db MAX_PATH dup (?)
+  LRing   db MAX_PATH dup (?)
+  LLittle db MAX_PATH dup (?)
+ends
+
+struct VOLATILE_SETTINGS
+  wszDeviceInstanceId du WINBIO_MAX_STRING_LEN dup (?)
+ends
+
+struct SETTINGS
+  GUI GUI_SETTINGS
+  cmd COMMAND_SETTINGS
+  vol VOLATILE_SETTINGS
+ends
+
+section '.text' code readable executable
+entry $
+                push SW_SHOWDEFAULT
+                call [GetCommandLineA]
+                push eax
+                push NULL
+                push NULL
+                call [GetModuleHandleA]
+                push eax
+                call WinMain
+
+                push eax
+                call [ExitProcess]
+
+;-------------------------------------------------------------------------;
+; WinMain
+;-------------------------------------------------------------------------;
+proc WinMain hInst:DWORD, hPrevInst:DWORD, szCmdLine:DWORD, nCmdShow:DWORD
+                local ini:SETTINGS
+                local wc:WNDCLASSEX
+                local msg:MSG
+                local icl:INITCOMMONCONTROLSEX
+                local rc:DWORD
+
+                push ebx
+                push esi
+                push edi
+
+                push 1
+                pop [rc]
+
+                ; Initialize common controls & OLE
+                push sizeof.INITCOMMONCONTROLSEX
+                push ICC_LISTVIEW_CLASSES or ICC_BAR_CLASSES
+                lea eax, [icl]
+                pop [icl.dwICC]
+                pop [icl.dwSize]
+                push eax
+                call [InitCommonControlsEx]
+                push NULL
+                call [CoInitialize]
+
+                ; Parse command-line arguments
+                call CommandLineInterface
+
+                ; Focus existing window
+                push NULL
+                push hotfinger.szClassName
+                call [FindWindowA]
+                test eax, eax
+                jz .not_found
+                push NULL
+                push NULL
+                push WM_NUDGE
+                push eax
+                call [SendMessageA]
+                push 2
+                pop eax
+                ret
+.not_found:
+                ; Read settings
+                lea ebx, [ini]
+                push ebx
+                call ReadAppDataSettings
+
+                lea eax, [ebx + SETTINGS.vol.wszDeviceInstanceId]
+                push eax
+                push guidDatabase
+                call GetSensorDeviceInstanceIdForDatabase
+                test eax, eax
+                jz .die
+
+;--------------------------------------------------------------------------;
+; Register window class
+;--------------------------------------------------------------------------;
+                xor eax, eax
+                lea ecx, [eax + sizeof.WNDCLASSEX]
+                push ecx
+                lea edi, [wc]
+                rep stosb
+                pop [wc.cbSize]
+                mov [wc.style], CS_HREDRAW or CS_VREDRAW or CS_BYTEALIGNWINDOW
+                mov [wc.lpfnWndProc], WndProc
+                push [hInst]
+                pop [wc.hInstance]
+
+                push IDI_MAIN
+                push [hInst]
+                call [LoadIconA]
+                mov [wc.hIcon], eax
+                mov [wc.hIconSm], eax
+
+                push IDC_ARROW
+                push NULL
+                call [LoadCursorA]
+                mov [wc.hCursor], eax
+
+                mov [wc.hbrBackground], COLOR_BTNFACE+1
+                mov [wc.lpszClassName], hotfinger.szClassName
+
+                lea eax, [wc]
+                push eax
+                call [RegisterClassExA]
+                test eax, eax
+                jnz .create_window
+
+                push MB_ICONERROR
+                push hotfinger.szError
+                push hotfinger.szWindowRegistrationFailed
+                push NULL
+                call [MessageBoxA]
+                jmp .die
+
+;--------------------------------------------------------------------------;
+; Create the window
+;--------------------------------------------------------------------------;
+.create_window: push ebx
+                push [hInst]
+                push NULL
+                push NULL
+
+                push [ini + SETTINGS.GUI.cy] ; height
+                mov ecx, [ini + SETTINGS.GUI.y]
+                sub [esp], ecx
+                push [ini + SETTINGS.GUI.cx] ; width
+                mov eax, [ini + SETTINGS.GUI.x]
+                sub [esp], eax
+
+                push ecx ; y
+                push eax ; x
+
+                push WS_OVERLAPPEDWINDOW
+                push hotfinger.szCaption
+                push hotfinger.szClassName
+                push 0
+                call [CreateWindowExA]
+                test eax, eax
+                jnz .show_window
+
+                push MB_ICONERROR
+                push hotfinger.szError
+                push hotfinger.szWindowCreationFailed
+                push eax ; NULL
+                call [MessageBoxA]
+                jmp .die
+
+.show_window:   xor esi, esi
+                xchg eax, esi
+                lea edi, [msg]
+                cmp [ini + SETTINGS.GUI.dwMinimized], eax
+                jnz .show_in_tray
+                push SW_SHOWMAXIMIZED
+                cmp [ini + SETTINGS.GUI.dwMaximized], eax
+                pop eax
+                cmovz eax, [nCmdShow]
+                push esi
+                push eax
+                push esi
+                call [ShowWindow]
+                call [UpdateWindow]
+                jmp .msg_loop
+
+.show_in_tray:  push SW_HIDE
+                push esi
+                call [ShowWindow]
+                push esi
+                call ShowTrayIcon
+
+;--------------------------------------------------------------------------;
+; Message loop
+;--------------------------------------------------------------------------;
+.msg_loop:      push 0
+                push 0
+                push NULL
+                push edi
+                call [GetMessageA]
+                test eax, eax
+                jz .finish
+
+                push edi
+                push esi
+                call [IsDialogMessage]
+                test eax, eax
+                jnz .msg_loop
+
+                push edi
+                push edi
+                call [TranslateMessage]
+                call [DispatchMessageA]
+                jmp .msg_loop
+.finish:        push [msg.wParam]
+                pop [rc]
+
+;--------------------------------------------------------------------------;
+; Finish
+;--------------------------------------------------------------------------;
+                cmp word [ini.vol.wszDeviceInstanceId], 0
+                jz .die ; uninstalled or something, do not save settings
+
+                push ebx
+                call WriteAppDataSettings
+
+.die:           call [CoUninitialize]
+
+                pop edi
+                pop esi
+                pop ebx
+                mov eax, [rc]
+                ret
+endp
+
+include 'cli.inc'
+include 'database.inc'
+include 'gui.inc'
+include 'sensor.inc'
+include 'settings.inc'
+include 'utils.inc'
+
+section '.data' data readable writeable
+include 'data.inc'
+
+section '.idata' import readable
+  library advapi32, 'advapi32.dll', \
+          comctl32, 'comctl32.dll', \
+          comdlg32, 'comdlg32.dll', \
+          gdi32, 'gdi32.dll', \
+          kernel32, 'kernel32.dll', \
+          ole32, 'ole32.dll', \
+          shell32, 'shell32.dll', \
+          shlwapi, 'shlwapi.dll', \
+          user32, 'user32.dll', \
+          winbio, 'winbio.dll'
+
+  import advapi32, \
+         CloseServiceHandle, 'CloseServiceHandle', \
+         ControlService, 'ControlService', \
+         OpenSCManagerA, 'OpenSCManagerA', \
+         OpenServiceA, 'OpenServiceA', \
+         QueryServiceStatus, 'QueryServiceStatus', \
+         RegCloseKey, 'RegCloseKey', \
+         RegCreateKeyExA, 'RegCreateKeyExA', \
+         RegDeleteKeyA, 'RegDeleteKeyA', \
+         RegEnumKeyExA, 'RegEnumKeyExA', \
+         RegEnumKeyExW, 'RegEnumKeyExW', \
+         RegGetValueA, 'RegGetValueA', \
+         RegOpenKeyExA, 'RegOpenKeyExA', \
+         RegOpenKeyExW, 'RegOpenKeyExW', \
+         RegSetValueExA, 'RegSetValueExA', \
+         StartServiceA, 'StartServiceA'
+
+  import comctl32, \
+         InitCommonControlsEx, 'InitCommonControlsEx'
+
+  import comdlg32, \
+         GetOpenFileNameA, 'GetOpenFileNameA'
+
+  import gdi32, \
+         GetStockObject, 'GetStockObject'
+
+  import kernel32, \
+         CloseHandle, 'CloseHandle', \
+         CreateDirectoryA, 'CreateDirectoryA', \
+         DeleteFileA, 'DeleteFileA', \
+         ExitProcess, 'ExitProcess', \
+         GetCommandLineA, 'GetCommandLineA', \
+         GetCommandLineW, 'GetCommandLineW', \
+         GetExitCodeProcess, 'GetExitCodeProcess', \
+         GetFileAttributesA, 'GetFileAttributesA', \
+         GetLastError, 'GetLastError', \
+         GetModuleFileNameW, 'GetModuleFileNameW', \
+         GetModuleHandleA, 'GetModuleHandleA', \
+         GetPrivateProfileIntA, 'GetPrivateProfileIntA', \
+         GetPrivateProfileStringA, 'GetPrivateProfileStringA', \
+         GetProcessHeap, 'GetProcessHeap', \
+         HeapAlloc, 'HeapAlloc', \
+         HeapFree, 'HeapFree', \
+         HeapReAlloc, 'HeapReAlloc', \
+         LocalFree, 'LocalFree', \
+         MulDiv, 'MulDiv', \
+         RemoveDirectoryA, 'RemoveDirectoryA', \
+         Sleep, 'Sleep', \
+         WaitForSingleObject, 'WaitForSingleObject', \
+         Wow64DisableWow64FsRedirection, 'Wow64DisableWow64FsRedirection', \
+         Wow64RevertWow64FsRedirection, 'Wow64RevertWow64FsRedirection', \
+         WritePrivateProfileStringA, 'WritePrivateProfileStringA', \
+         lstrcmpW, 'lstrcmpW', \
+         lstrcmpiW, 'lstrcmpiW', \
+         lstrlenA, 'lstrlenA', \
+         lstrlenW, 'lstrlenW'
+
+  import ole32, \
+         CoInitialize, 'CoInitialize', \
+         CoUninitialize, 'CoUninitialize'
+
+  import shell32, \
+         CommandLineToArgvW, 'CommandLineToArgvW', \
+         SHGetFileInfo, 'SHGetFileInfo', \
+         SHGetFolderPathA, 'SHGetFolderPathA', \
+         Shell_NotifyIconA, 'Shell_NotifyIconA', \
+         ShellExecuteA, 'ShellExecuteA', \
+         ShellExecuteExW, 'ShellExecuteExW'
+
+  import shlwapi, \
+         SHAutoComplete, 'SHAutoComplete'
+
+  import user32, \
+        AdjustWindowRectExForDpi, 'AdjustWindowRectExForDpi', \
+        AppendMenuA, 'AppendMenuA', \
+        CreateMenu, 'CreateMenu', \
+        CreatePopupMenu, 'CreatePopupMenu', \
+        CreateWindowExA, 'CreateWindowExA', \
+        DefWindowProcA, 'DefWindowProcA', \
+        DestroyMenu, 'DestroyMenu', \
+        DestroyWindow, 'DestroyWindow', \
+        DialogBoxParamA, 'DialogBoxParamA', \
+        DispatchMessageA, 'DispatchMessageA', \
+        DrawMenuBar, 'DrawMenuBar', \
+        EnableMenuItem, 'EnableMenuItem', \
+        EnableWindow, 'EnableWindow', \
+        EndDialog, 'EndDialog', \
+        FindWindowA, 'FindWindowA', \
+        GetClientRect, 'GetClientRect', \
+        GetCursorPos, 'GetCursorPos', \
+        GetDlgItem, 'GetDlgItem', \
+        GetDpiForSystem, 'GetDpiForSystem', \
+        GetDpiForWindow, 'GetDpiForWindow', \
+        GetMenuItemCount, 'GetMenuItemCount', \
+        GetMessageA, 'GetMessageA', \
+        GetSystemMetricsForDpi, 'GetSystemMetricsForDpi', \
+        GetWindowLongA, 'GetWindowLongA', \
+        GetWindowRect, 'GetWindowRect', \
+        GetWindowTextA, 'GetWindowTextA', \
+        GetWindowTextLengthA, 'GetWindowTextLengthA', \
+        IsDialogMessage, 'IsDialogMessage', \
+        LoadCursorA, 'LoadCursorA', \
+        LoadIconA, 'LoadIconA', \
+        MapDialogRect, 'MapDialogRect', \
+        MessageBoxA, 'MessageBoxA', \
+        MoveWindow, 'MoveWindow', \
+        PostQuitMessage, 'PostQuitMessage', \
+        RedrawWindow, 'RedrawWindow', \
+        RegisterClassExA, 'RegisterClassExA', \
+        SendDlgItemMessageA, 'SendDlgItemMessageA', \
+        SendMessageA, 'SendMessageA', \
+        SetFocus, 'SetFocus', \
+        SetForegroundWindow, 'SetForegroundWindow', \
+        SetMenu, 'SetMenu', \
+        SetWindowLongA, 'SetWindowLongA', \
+        SetWindowPos, 'SetWindowPos', \
+        SetWindowTextA, 'SetWindowTextA', \
+        ShowWindow, 'ShowWindow', \
+        TrackPopupMenu, 'TrackPopupMenu', \
+        TranslateMessage, 'TranslateMessage', \
+        UpdateWindow, 'UpdateWindow', \
+        wsprintfA, 'wsprintfA'
+
+  import winbio, \
+        WinBioAsyncEnumBiometricUnits, 'WinBioAsyncEnumBiometricUnits', \
+        WinBioAsyncOpenFramework, 'WinBioAsyncOpenFramework', \
+        WinBioAsyncOpenSession, 'WinBioAsyncOpenSession', \
+        WinBioCancel,'WinBioCancel', \
+        WinBioCloseFramework, 'WinBioCloseFramework', \
+        WinBioCloseSession, 'WinBioCloseSession', \
+        WinBioDeleteTemplate, 'WinBioDeleteTemplate', \
+        WinBioEnrollBegin, 'WinBioEnrollBegin', \
+        WinBioEnrollCapture, 'WinBioEnrollCapture', \
+        WinBioEnrollCommit, 'WinBioEnrollCommit', \
+        WinBioEnrollDiscard, 'WinBioEnrollDiscard', \
+        WinBioEnumBiometricUnits, 'WinBioEnumBiometricUnits', \
+        WinBioFree, 'WinBioFree', \
+        WinBioIdentify, 'WinBioIdentify', \
+        WinBioLocateSensor, 'WinBioLocateSensor', \
+        WinBioWait, 'WinBioWait'
+
+section '.rsrc' resource data readable
+
+; Dialogs
+IDD_SENSOR = 100
+IDD_ABOUT  = 101
+
+; Sensor dialog
+IDD_SENSOR_WIDTH  = 350
+IDD_SENSOR_HEIGHT = 120
+
+IDD_SENSOR_WIDTH_MIN  = 250
+IDD_SENSOR_HEIGHT_MIN = 80
+
+IDC_BTN_SELECT     = 2000
+IDC_BTN_CANCEL     = 2001
+IDC_STATIC_MESSAGE = 2002
+IDC_LIST_SENSORS   = 2003
+
+; About dialog
+IDC_SYSLINK = 3000
+
+; Icons
+IDI_MAIN = 401
+
+  directory RT_DIALOG, dialogs, \
+            RT_ICON, icons, \
+            RT_GROUP_ICON, group_icons, \
+            RT_MANIFEST, manifests, \
+            RT_VERSION, versions
+
+  resource dialogs, \
+           IDD_SENSOR, LANG_ENGLISH+SUBLANG_DEFAULT, sensor_dialog, \
+           IDD_ABOUT, LANG_ENGLISH+SUBLANG_DEFAULT, about_dialog
+  resource icons, 1, LANG_NEUTRAL, icon_data
+  resource group_icons, IDI_MAIN, LANG_NEUTRAL, main_icon
+  resource manifests, 1, LANG_NEUTRAL, manifest
+  resource versions, 1, LANG_NEUTRAL, version
+
+  dialog sensor_dialog, 'Sensor dialog', 0,0,IDD_SENSOR_WIDTH,IDD_SENSOR_HEIGHT, WS_POPUP+WS_OVERLAPPEDWINDOW+DS_MODALFRAME+DS_CENTER
+    dialogitem 'BUTTON','&Select', IDC_BTN_SELECT, IDD_SENSOR_WIDTH-108,IDD_SENSOR_HEIGHT-18,50,14, BS_DEFPUSHBUTTON+WS_VISIBLE+WS_TABSTOP+WS_DISABLED
+    dialogitem 'BUTTON','&Cancel', IDC_BTN_CANCEL, IDD_SENSOR_WIDTH-54,IDD_SENSOR_HEIGHT-18,50,14, BS_PUSHBUTTON+WS_VISIBLE+WS_TABSTOP
+    dialogitem 'STATIC','Select sensor from the list', IDC_STATIC_MESSAGE, 4,IDD_SENSOR_HEIGHT-14,IDD_SENSOR_WIDTH-112,8, WS_VISIBLE
+    dialogitem 'SysListView32','', IDC_LIST_SENSORS, 4,4,IDD_SENSOR_WIDTH-8,IDD_SENSOR_HEIGHT-24, WS_VISIBLE+WS_BORDER+WS_TABSTOP+LVS_REPORT+LVS_SINGLESEL+LVS_SHOWSELALWAYS
+  enddialog
+
+  dialog about_dialog, 'About', 40,40,160,55, WS_CAPTION+WS_POPUP+WS_SYSMENU+DS_MODALFRAME
+    dialogitem 'STATIC', <'HotFinger application launcher'>, -1, 36,10,120,10, WS_VISIBLE
+    dialogitem 'SysLink', <'<a>https://github.com/resilar/HotFinger</a>'>, IDC_SYSLINK, 36,20,120,10, WS_VISIBLE
+    dialogitem 'STATIC', IDI_MAIN, -1, 8,8,32,32, WS_VISIBLE+SS_ICON
+    dialogitem 'STATIC', '', -1, 4,35,152,11, WS_VISIBLE+SS_ETCHEDHORZ
+    dialogitem 'STATIC', <'HotFinger v0.1.0'>, -1, 4,42,100,10, WS_VISIBLE+SS_LEFT
+    dialogitem 'BUTTON', 'OK', IDOK, 114,38,42,14, WS_VISIBLE+WS_TABSTOP+BS_DEFPUSHBUTTON
+  enddialog
+
+  icon main_icon, icon_data, 'sysclass.ico'
+
+  resdata manifest
+    db '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>',0dh,0ah
+    db '<assembly xmlns="urn:schemas-microsoft-com:asm.v1" xmlns:asmv3="urn:schemas-microsoft-com:asm.v3" manifestVersion="1.0">',0dh,0ah
+    db ' <assemblyIdentity version="1.0.0.0" processorArchitecture="x86" type="win32" name="hotfinger.exe"/>',0dh,0ah
+    db ' <description>HotFinger</description>',0dh,0ah
+    db ' <dependency>',0dh,0ah
+    db '  <dependentAssembly>',0dh,0ah
+    db '    <assemblyIdentity type="win32" name="Microsoft.Windows.Common-Controls" version="6.0.0.0" processorArchitecture="x86" publicKeyToken="6595b64144ccf1df" language="*"/> ',0dh,0ah
+    db '  </dependentAssembly>',0dh,0ah
+    db ' </dependency>',0dh,0ah
+    db ' <asmv3:application>',0dh,0ah
+    db '  <asmv3:windowsSettings xmlns="http://schemas.microsoft.com/SMI/2005/WindowsSettings">',0dh,0ah
+;    db '   <dpiAwareness xmlns="http://schemas.microsoft.com/SMI/2016/WindowsSettings">PerMonitorV2</dpiAwareness>',0dh,0ah
+    db '   <dpiAware>true</dpiAware>',0dh,0ah
+    db '  </asmv3:windowsSettings>',0dh,0ah
+    db ' </asmv3:application>',0dh,0ah
+    db '</assembly>',0dh,0ah
+  endres
+
+  versioninfo version, \
+    VOS__WINDOWS32, VFT_APP,VFT2_UNKNOWN, LANG_ENGLISH+SUBLANG_DEFAULT, 0, \
+    'FileDescription',  'Biometric hotkeys', \
+    'FileVersion',      '0.1.0.0', \
+    'ProductName',      'HotFinger', \
+    'ProductVersion',   '0.1.0', \
+    'LegalCopyright',   'UNLICENSE', \
+    'OriginalFilename', 'hotfinger.exe'
